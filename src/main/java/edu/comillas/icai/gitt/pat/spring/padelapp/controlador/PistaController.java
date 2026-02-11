79d358126623ef2b08b70605955fb8c671fd7c4b
nachoyubero <202310138@alu.comillas.edu>
2026-02-11 13:07:00 +0100

    "Se crean las carpetas modelo y controlador y se inicializa PistaController"

---
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/padelapp/controlador/PistaController.java
@@ -0,0 +1,15 @@
+package edu.comillas.icai.gitt.pat.spring.padelapp.controlador;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Aqui se van a realizar los siguientes endpoints:
+//
+@RestController
+@RequestMapping("/pistaPadel") //Comenzamos a definir nuestra base de URL
+public class PistaController {
+
+
+
+}
