e7fb44e97a1d2a6b197f2c8dcf0f4629c0053840
Mateo <mateo.mascaraque@gmail.com>
2026-02-18 15:47:08 +0100

    getters y constructor de franjaHoraria

--- src/main/java/edu/comillas/icai/gitt/pat/spring/padelapp/clases/FranjaHoraria.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/padelapp/clases/FranjaHoraria.java
@@ -4,6 +4,22 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 
 public class FranjaHoraria {
-        LocalTime inicio;
-        LocalTime fin;
+        private LocalTime inicio;
+        private LocalTime fin;
+
+        // Constructor necesario para crear objetos desde el controlador
+        public FranjaHoraria(LocalTime inicio, LocalTime fin) {
+                this.inicio = inicio;
+                this.fin = fin;
+        }
+
+        // Getters para que Spring pueda convertir el objeto a JSON
+        public LocalTime getInicio() {
+                return inicio;
+        }
+
+        public LocalTime getFin() {
+                return fin;
+        }
+}
 }
