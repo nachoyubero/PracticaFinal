587d1f723859200fce012cae029b511f7306e539
ignaciogutigar <202302203@alu.comillas.edu>
2026-02-18 23:36:52 +0100

    trazas

--- src/main/java/edu/comillas/icai/gitt/pat/spring/padelapp/controlador/PistaController.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/padelapp/controlador/PistaController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.time.LocalDate;
